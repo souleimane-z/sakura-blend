a5162fd0e79caae9962d75de0875a93414995f6d
souleimane-z <souleimane.zeggai@gmail.com>
2024-12-18 16:21:27 +0100

    menu.php images

--- includes/menu.php
+++ includes/menu.php
@@ -5,141 +5,238 @@ return [
             'nom' => 'Edamame',
             'description' => 'Fèves de soja vapeur, sel de mer',
             'prix' => 6.50,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'Edamame.svg'
         ],
         [
             'nom' => 'Gyoza Végétariens',
             'description' => 'Raviolis japonais aux légumes (6 pièces)',
             'prix' => 8.90,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'Gyoza.svg'
         ],
         [
             'nom' => 'Miso Soup',
             'description' => 'Soupe traditionnelle, tofu, algues wakame',
             'prix' => 5.50,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'Miso-Soup.svg'
         ],
         [
             'nom' => 'Wakame Sarada',
             'description' => 'Salade d\'algues au sésame',
             'prix' => 7.90,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'Seaweed.svg'
         ],
         [
             'nom' => 'Tempura Mix',
             'description' => 'Assortiment de crevettes et légumes en tempura',
             'prix' => 12.90,
-            'vegetarien' => false
+            'vegetarien' => false,
+            'image' => 'Tempura.svg'
         ]
     ],
+
     'plats' => [
         [
             'nom' => 'Ramen Miso Végétal',
             'description' => 'Nouilles, bouillon miso, œuf mollet, champignons, légumes',
             'prix' => 18.90,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'Ramen.svg'
         ],
         [
             'nom' => 'Chirashi Salmon',
             'description' => 'Bol de riz vinaigré, saumon frais, avocat, mangue, œufs de poisson',
             'prix' => 22.50,
-            'vegetarien' => false
+            'vegetarien' => false,
+            'image' => 'CHIRASHI.png'
         ],
         [
             'nom' => 'Wagyu Don',
             'description' => 'Riz, bœuf wagyu grillé, oignon caramélisé, œuf onsen',
             'prix' => 29.90,
-            'vegetarien' => false
+            'vegetarien' => false,
+            'image' => 'wagyu.svg'
         ],
         [
             'nom' => 'Tempura Udon',
             'description' => 'Nouilles udon chaudes, tempura de crevettes, bouillon dashi',
             'prix' => 19.90,
-            'vegetarien' => false
+            'vegetarien' => false,
+            'image' => 'tempura-udon.svg'
         ],
         [
             'nom' => 'Teriyaki Poulet',
             'description' => 'Poulet grillé sauce teriyaki, riz, légumes de saison',
             'prix' => 17.90,
-            'vegetarien' => false
+            'vegetarien' => false,
+            'image' => 'Teriyaki.svg'
         ]
     ],
+
+    'sushis' => [
+        [
+            'nom' => 'Maguro Nigiri',
+            'description' => 'Sushi au thon rouge traditionnel',
+            'prix' => 4.90,
+            'vegetarien' => false,
+            'image' => 'Maguro-Nigiri.webp'
+        ],
+        [
+            'nom' => 'Sake Nigiri',
+            'description' => 'Sushi au saumon frais',
+            'prix' => 4.50,
+            'vegetarien' => false,
+            'image' => 'Sake-Nigiri.png'
+        ],
+        [
+            'nom' => 'California Roll',
+            'description' => 'Avocat, crabe, concombre et masago (8 pièces)',
+            'prix' => 12.90,
+            'vegetarien' => false,
+            'image' => 'California-Roll.webp'
+        ],
+        [
+            'nom' => 'Inari',
+            'description' => 'Tofu frit mariné farci de riz vinaigré',
+            'prix' => 3.90,
+            'vegetarien' => true,
+            'image' => 'Inari.webp'
+        ],
+        [
+            'nom' => 'Tamago Nigiri',
+            'description' => 'Sushi à l\'omelette japonaise sucrée',
+            'prix' => 3.50,
+            'vegetarien' => true,
+            'image' => 'Tamago-Nigiri.png'
+        ],
+        [
+            'nom' => 'Rainbow Roll',
+            'description' => 'California roll recouvert de poissons variés (8 pièces)',
+            'prix' => 15.90,
+            'vegetarien' => false,
+            'image' => 'Rainbow-Roll.webp'
+        ],
+        [
+            'nom' => 'Spicy Tuna Roll',
+            'description' => 'Thon épicé, avocat, sauce pimentée (6 pièces)',
+            'prix' => 13.90,
+            'vegetarien' => false,
+            'image' => 'Spicy-Tuna-Roll.webp'
+        ],
+        [
+            'nom' => 'Avocado Roll',
+            'description' => 'Avocat, concombre, carotte (6 pièces)',
+            'prix' => 9.90,
+            'vegetarien' => true,
+            'image' => 'Avocado-Roll.png'
+        ],
+        [
+            'nom' => 'Tempura Roll',
+            'description' => 'Crevette tempura, avocat, sauce teriyaki (6 pièces)',
+            'prix' => 12.90,
+            'vegetarien' => false,
+            'image' => 'Tempura-Roll.png'
+        ]
+    ],
+
     'desserts' => [
         [
             'nom' => 'Mochi Glacé',
             'description' => 'Assortiment de 3 mochis (thé vert, mangue, sésame noir)',
             'prix' => 8.50,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'frozen-mochi.webp'
         ],
         [
             'nom' => 'Dorayaki',
             'description' => 'Pancakes japonais fourrés à la pâte de haricot rouge',
             'prix' => 7.50,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'dorayaki.png'
         ],
         [
             'nom' => 'Matcha Tiramisu',
             'description' => 'Tiramisu au thé vert matcha',
             'prix' => 8.90,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'matcha_tiramisu.png'
         ],
         [
             'nom' => 'Taiyaki',
             'description' => 'Gaufre en forme de poisson, glace vanille, sauce chocolat',
             'prix' => 8.90,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'Taiyaki.webp'
         ],
         [
             'nom' => 'Cheesecake Yuzu',
             'description' => 'Cheesecake japonais au citron yuzu',
             'prix' => 9.50,
-            'vegetarien' => true
+            'vegetarien' => true,
+            'image' => 'Cheesecake-yuzu.png'
         ]
     ],
+
     'thes' => [
         [
             'nom' => 'Sencha',
             'description' => 'Thé vert japonais traditionnel',
-            'prix' => 4.50
+            'prix' => 4.50,
+            'image' => 'Sencha.webp'
         ],
         [
             'nom' => 'Gyokuro',
             'description' => 'Thé vert premium à l\'umami prononcé',
-            'prix' => 6.50
+            'prix' => 6.50,
+            'image' => 'Gyokuro.webp'
         ],
         [
             'nom' => 'Hojicha',
             'description' => 'Thé vert torréfié aux notes caramélisées',
-            'prix' => 4.90
+            'prix' => 4.90,
+            'image' => 'Hojicha.png'
+        ],
+        [
+            'nom' => 'Matcha Cérémoniale',
+            'description' => 'Poudre de thé vert finement moulue aux notes herbales',
+            'prix' => 9.90,
+            'image' => 'Matcha.webp'
         ],
         [
             'nom' => 'Genmaicha',
             'description' => 'Thé vert au riz grillé',
-            'prix' => 4.90
+            'prix' => 4.90,
+            'image' => 'Genmaicha.png'
         ]
     ],
+
     'boissons' => [
         [
             'nom' => 'Ramune',
             'description' => 'Limonade japonaise',
-            'prix' => 4.50
+            'prix' => 4.50,
+            'image' => 'Ramune.webp'
         ],
         [
             'nom' => 'Calpis',
             'description' => 'Boisson lactée japonaise',
-            'prix' => 4.50
+            'prix' => 4.50,
+            'image' => 'Calpis.png'
         ],
         [
             'nom' => 'Thé Glacé Maison',
             'description' => 'Thé vert glacé au yuzu',
-            'prix' => 4.90
+            'prix' => 4.90,
+            'image' => 'yuzu-ice-tea.png'
         ],
         [
             'nom' => 'Jus de Fruits',
             'description' => 'Litchi / Mangue / Yuzu',
-            'prix' => 4.50
+            'prix' => 4.50,
+            'image' => 'fruit-juice.png'
         ]
     ]
 ];
-
-?>
--- menu.php
+++ menu.php
@@ -15,6 +15,7 @@ $menu = include __DIR__ . '/includes/menu.php';
                     <ul>
                         <li><a class="filter active" data-filter=".entrees">Entrées</a></li>
                         <li><a class="filter" data-filter=".plats">Plats</a></li>
+                        <li><a class="filter" data-filter=".sushis">sushis</a></li>
                         <li><a class="filter" data-filter=".desserts">Desserts</a></li>
                         <li><a class="filter" data-filter=".thes">Thés</a></li>
                         <li><a class="filter" data-filter=".boissons">Boissons</a></li>
@@ -26,72 +27,160 @@ $menu = include __DIR__ . '/includes/menu.php';
                     <!-- Entrées -->
                     <?php foreach($menu['entrees'] as $item): ?>
                         <div class="entrees menu-restaurant">
-                       <span class="clearfix">
-                           <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
-                           <span class="menu-line"></span>
-                           <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
-                       </span>
-                            <span class="menu-subtitle">
-                           <?php echo $item['description']; ?>
-                                <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
-                                    <i class="fa-solid fa-leaf" title="Végétarien"></i>
-                                <?php endif; ?>
-                       </span>
+                            <div class="menu-item-header">
+
+                                <img src="./assets/images/Menus/Entrée/<?php echo $item['image']; ?>"
+                                     alt="<?php echo $item['nom']; ?>"
+                                     class="menu-item-image">
+
+                                <div class="menu-item-content">
+                                    <span class="clearfix">
+                                        <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
+                                        <span class="menu-line"></span>
+                                        <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
+                                    </span>
+                                        <span class="menu-subtitle">
+                                        <?php echo $item['description']; ?>
+                                        <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
+                                            <i class="fa-solid fa-leaf" title="Végétarien"></i>
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     <?php endforeach; ?>
 
                     <!-- Plats -->
                     <?php foreach($menu['plats'] as $item): ?>
                         <div class="plats menu-restaurant">
-                       <span class="clearfix">
-                           <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
-                           <span class="menu-line"></span>
-                           <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
-                       </span>
-                            <span class="menu-subtitle">
-                           <?php echo $item['description']; ?>
-                                <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
-                                    <i class="fa-solid fa-leaf" title="Végétarien"></i>
-                                <?php endif; ?>
-                       </span>
+                            <div class="menu-item-header">
+
+                                <img src="./assets/images/Menus/Plats/<?php echo $item['image']; ?>"
+                                     alt="<?php echo $item['nom']; ?>"
+                                     class="menu-item-image">
+
+                                <div class="menu-item-content">
+                                    <span class="clearfix">
+                                        <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
+                                        <span class="menu-line"></span>
+                                        <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
+                                    </span>
+                                    <span class="menu-subtitle">
+                                        <?php echo $item['description']; ?>
+                                        <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
+                                            <i class="fa-solid fa-leaf" title="Végétarien"></i>
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+
+                    <!-- Sushis -->
+                    <?php foreach($menu['sushis'] as $item): ?>
+                        <div class="sushis menu-restaurant">
+                            <div class="menu-item-header">
+
+                                <img src="./assets/images/Menus/Sushis/<?php echo $item['image']; ?>"
+                                     alt="<?php echo $item['nom']; ?>"
+                                     class="menu-item-image">
+
+                                <div class="menu-item-content">
+                                    <span class="clearfix">
+                                        <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
+                                        <span class="menu-line"></span>
+                                        <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
+                                    </span>
+                                    <span class="menu-subtitle">
+                                        <?php echo $item['description']; ?>
+                                        <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
+                                            <i class="fa-solid fa-leaf" title="Végétarien"></i>
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     <?php endforeach; ?>
 
                     <!-- Desserts -->
                     <?php foreach($menu['desserts'] as $item): ?>
                         <div class="desserts menu-restaurant">
-                       <span class="clearfix">
-                           <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
-                           <span class="menu-line"></span>
-                           <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
-                       </span>
-                            <span class="menu-subtitle"><?php echo $item['description']; ?></span>
+                            <div class="menu-item-header">
+
+                                <img src="./assets/images/Menus/Desserts/<?php echo $item['image']; ?>"
+                                     alt="<?php echo $item['nom']; ?>"
+                                     class="menu-item-image">
+
+                                <div class="menu-item-content">
+                                    <span class="clearfix">
+                                        <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
+                                        <span class="menu-line"></span>
+                                        <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
+                                    </span>
+                                    <span class="menu-subtitle">
+                                        <?php echo $item['description']; ?>
+                                        <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
+                                            <i class="fa-solid fa-leaf" title="Végétarien"></i>
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     <?php endforeach; ?>
+
 
                     <!-- Thés -->
                     <?php foreach($menu['thes'] as $item): ?>
                         <div class="thes menu-restaurant">
-                       <span class="clearfix">
-                           <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
-                           <span class="menu-line"></span>
-                           <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
-                       </span>
-                            <span class="menu-subtitle"><?php echo $item['description']; ?></span>
+                            <div class="menu-item-header">
+
+                                <img src="./assets/images/Menus/Tea/<?php echo $item['image']; ?>"
+                                     alt="<?php echo $item['nom']; ?>"
+                                     class="menu-item-image">
+
+                                <div class="menu-item-content">
+                                    <span class="clearfix">
+                                        <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
+                                        <span class="menu-line"></span>
+                                        <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
+                                    </span>
+                                    <span class="menu-subtitle">
+                                        <?php echo $item['description']; ?>
+                                        <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
+                                            <i class="fa-solid fa-leaf" title="Végétarien"></i>
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     <?php endforeach; ?>
 
                     <!-- Boissons -->
                     <?php foreach($menu['boissons'] as $item): ?>
                         <div class="boissons menu-restaurant">
-                       <span class="clearfix">
-                           <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
-                           <span class="menu-line"></span>
-                           <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
-                       </span>
-                            <span class="menu-subtitle"><?php echo $item['description']; ?></span>
+                            <div class="menu-item-header">
+
+                                <img src="./assets/images/Menus/Boissons/<?php echo $item['image']; ?>"
+                                     alt="<?php echo $item['nom']; ?>"
+                                     class="menu-item-image">
+
+                                <div class="menu-item-content">
+                                    <span class="clearfix">
+                                        <a class="menu-title" href="#"><?php echo $item['nom']; ?></a>
+                                        <span class="menu-line"></span>
+                                        <span class="menu-price"><?php echo number_format($item['prix'], 2); ?>€</span>
+                                    </span>
+                                    <span class="menu-subtitle">
+                                        <?php echo $item['description']; ?>
+                                        <?php if(isset($item['vegetarien']) && $item['vegetarien']): ?>
+                                            <i class="fa-solid fa-leaf" title="Végétarien"></i>
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     <?php endforeach; ?>
+
                 </div>
             </div>
         </div>
